b4be5b2ef67be6c030bd7bae407b8bd2c71aacbb
unknown <gregmasterbr@gmail.com>
2022-11-12 23:09:40 -0300

    Funcionalidade para enviar o email. Adicionado uma tabela, adicionado um template e alterado o template da vaga para exibir os emails disparados para aquela vaga.

--- README.md
+++ README.md
@@ -9,3 +9,18 @@ Sistema para gerenciar processo seletivos.
 * [Aula 2](https://grizzly-amaranthus-f6a.notion.site/PYLAB-2022-AULA-2-c1a20954bd8d4272b4c95b22fd5ace6b)
 * [Aula 3 ](https://grizzly-amaranthus-f6a.notion.site/PYLAB-2022-AULA-3-644c7840a2a949ffb51eef088aabf93f)
 
+## Materiais Complementares
+[https://www.youtube.com/watch?v=JSwofXOC4OY](https://www.youtube.com/watch?v=JSwofXOC4OY)
+
+Gerar credenciais do GMAIL:
+
+[https://medium.com/@heltonteixeira92/enviando-e-mail-com-django-e-uma-conta-gmail-atualizado-2022-bc2f186811ec](https://medium.com/@heltonteixeira92/enviando-e-mail-com-django-e-uma-conta-gmail-atualizado-2022-bc2f186811ec)
+
+
+## Desafios
+
+Para treinar suas habilidades realizee esses dois 2 desafios.
+
+→ Permitir alteração do status de uma vaga
+
+→ Desenvolver funcionalidades das tecnologias que domina ou não.
--- seletive/vagas/views.py
+++ seletive/vagas/views.py
@@ -1,11 +1,17 @@
 
 from django.shortcuts import render, redirect, get_object_or_404, Http404
 from seletive.empresa.models import Vagas
-from .models import Tarefa
+from .models import Tarefa, Emails
 from django.urls import reverse
 #Django Messages - definida lá no settings - MESSAGES_TAG
 from django.contrib import messages
 from django.contrib.messages import constants
+
+#envia email
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+from django.core.mail import EmailMultiAlternatives
+from django.conf import settings
 
 # Create your views here.
 def nova_vaga(request):
@@ -44,22 +50,10 @@ def nova_vaga(request):
 def vaga(request, id):
     vaga = get_object_or_404(Vagas, id=id)
     tarefas = Tarefa.objects.filter(vaga=vaga).filter(realizada=False)
+    emails = Emails.objects.filter(vaga=vaga)
     return render(request, 'vaga.html', {'vaga': vaga,
-                                         'tarefas': tarefas,})     
-
-
-def realizar_tarefa(request, id):
-    tarefas_list = Tarefa.objects.filter(id=id).filter(realizada=False)
-
-    if not tarefas_list.exists():
-        messages.add_message(request, constants.ERROR, 'Erro interno do sistema!')
-        return redirect(f'/home/empresas/')
-
-    tarefa = tarefas_list.first()
-    tarefa.realizada = True
-    tarefa.save()    
-    messages.add_message(request, constants.SUCCESS, 'Tarefa realizada com sucesso, parabéns!')
-    return redirect(f'/vagas/vaga/{tarefa.vaga.id}')
+                                         'tarefas': tarefas,
+                                         'emails': emails,})     
 
 def nova_tarefa(request, id_vaga):
     titulo = request.POST.get('titulo')
@@ -76,3 +70,50 @@ def nova_tarefa(request, id_vaga):
     except: 
         messages.add_message(request, constants.ERROR, 'Erro interno no sistema, impossibilitou de cadastrar uma nova tarefa. Tente novamente em alguns instantes!')
         return redirect(f'/vagas/vaga/{id_vaga}') 
+
+
+def realizar_tarefa(request, id):
+    tarefas_list = Tarefa.objects.filter(id=id).filter(realizada=False)
+
+    if not tarefas_list.exists():
+        messages.add_message(request, constants.ERROR, 'Baixe tarefas atribuída exclusivamente para a vaga !')
+        return redirect(f'/home/empresas/')
+
+    tarefa = tarefas_list.first()
+    tarefa.realizada = True
+    tarefa.save()    
+    messages.add_message(request, constants.SUCCESS, 'Tarefa realizada com sucesso, parabéns!')
+    return redirect(f'/vagas/vaga/{tarefa.vaga.id}')        
+
+
+
+def envia_email(request, id_vaga):
+    vaga = Vagas.objects.get(id=id_vaga)
+    assunto = request.POST.get('assunto')
+    corpo = request.POST.get('corpo')
+
+    html_content = render_to_string('emails/template_email.html', {'corpo': corpo})
+    text_content = strip_tags(html_content)
+    email = EmailMultiAlternatives(assunto, text_content, settings.EMAIL_HOST_USER, [vaga.email,])
+    email.attach_alternative(html_content, "text/html")
+    
+    if email.send():
+        mail = Emails(
+            vaga=vaga,
+            assunto=assunto,
+            corpo=corpo,
+            enviado=True
+        )
+        mail.save()
+        messages.add_message(request, constants.SUCCESS, 'Email enviado com sucesso.')
+        return redirect(f'/vagas/vaga/{id_vaga}')
+    else:
+        mail = Emails(
+            vaga=vaga,
+            assunto=assunto,
+            corpo=corpo,
+            enviado=False
+        )
+        mail.save()
+        messages.add_message(request, constants.ERROR, 'Erro interno do sistema!')
+        return redirect(f'/vagas/vaga/{id_vaga}')
